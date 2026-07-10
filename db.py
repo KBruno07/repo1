a41394f10766d1ad03d1922358b4b883f73549c3
KBruno07 <kosztyubruno1@gmail.com>
2026-07-10 16:28:42 +0200

    Kurzor létrehozása

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 import pymysql
 mydb = pymysql.connect(
     host="192.168.0.124",
-    user="Kiberbiztos",
-    password="Kiberbiztos007!",
+    user="newuser",
+    password="stop1",
     database="finance_tracker"
 )
 print(mydb)
